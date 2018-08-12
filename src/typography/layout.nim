ae4fac35bd9a82da2f399236130c3d03133998d9
treeform <starplant@gmail.com>
2018-08-12 14:54:54 -0700

    Make drawText simpler.

--- src/typography/layout.nim
+++ src/typography/layout.nim
@@ -30,41 +30,6 @@ proc kerningAdjustment*(font: var Font, prev, c: string): float =
       return - kerning * scale
 
 
-proc drawText*(font: var Font, image: var Image, start: Vec2, text: string) =
-  ## Draw text string
-  var at = start
-  var prev = ""
-  var fontHeight = font.ascent - font.descent
-  var scale = font.size / fontHeight
-  at.y += font.size
-  for rune in runes(text):
-    let c = $rune
-    if rune == Rune(10):
-      at.x = start.x
-      at.y += font.lineHeight
-      continue
-
-    if c in font.glyphs:
-      var glyph = font.glyphs[c]
-
-      at.x += font.kerningAdjustment(prev, c)
-
-      var glyphOffset: Vec2
-      var subPixelShift = at.x - floor(at.x)
-      var img = font.getGlyphImage(glyph, glyphOffset, subPixelShift=subPixelShift)
-      image.blit(
-        img,
-        rect(0, 0, img.width, img.height),
-        rect(int(at.x + glyphOffset.x), int(at.y + glyphOffset.y), img.width, img.height)
-      )
-      at.x += glyph.advance * scale
-
-      prev = c
-
-    else:
-      discard
-      #echo "missing", $c
-
 
 proc typeset*(font: var Font, text: string, start: Vec2 = vec2(0, 0)): seq[GlyphPosition] =
   ## Draw text string
@@ -114,3 +79,9 @@ proc drawText*(image: var Image, layout: seq[GlyphPosition]) =
       rect(0, 0, img.width, img.height),
       rect(int(pos.at.x + glyphOffset.x), int(pos.at.y + glyphOffset.y), img.width, img.height)
     )
+
+
+proc drawText*(font: var Font, image: var Image, start: Vec2, text: string) =
+  ## Draw text string
+  var layout = font.typeset(text, start=start)
+  image.drawText(layout)
--- tests/test.nim
+++ tests/test.nim
@@ -16,120 +16,117 @@ proc alphaWhite(image: var Image) =
       c.a = 255
       image.putrgba(x, y, c)
 
-# block:
-#   var image = newImage(500, 40, 4)
-#   var font = readFontSvg("fonts/Ubuntu.svg")
-#   font.size = 16
+block:
+  var image = newImage(500, 40, 4)
+  var font = readFontSvg("fonts/Ubuntu.svg")
+  font.size = 16
 
-#   font.drawText(image, vec2(10, 10), "The quick brown fox jumps over the lazy dog.")
+  font.drawText(image, vec2(10, 10), "The quick brown fox jumps over the lazy dog.")
 
-#   image.alphaWhite()
-#   image.save("basicSvg.png")
+  image.alphaWhite()
+  image.save("basicSvg.png")
 
-# block:
-#   var font = readFontTtf("fonts/Ubuntu.ttf")
-#   font.size = 16
-#   var image = newImage(500, 40, 4)
+block:
+  var font = readFontTtf("fonts/Ubuntu.ttf")
+  font.size = 16
+  var image = newImage(500, 40, 4)
 
-#   font.drawText(image, vec2(10, 10), "The quick brown fox jumps over the lazy dog.")
+  font.drawText(image, vec2(10, 10), "The quick brown fox jumps over the lazy dog.")
 
-#   image.alphaWhite()
-#   image.save("basicTtf.png")
+  image.alphaWhite()
+  image.save("basicTtf.png")
 
-# block:
-#   var font = readFontSvg("fonts/Ubuntu.svg")
-#   var image = newImage(500, 240, 4)
+block:
+  var font = readFontSvg("fonts/Ubuntu.svg")
+  var image = newImage(500, 240, 4)
 
-#   font.size = 8
-#   font.drawText(image, vec2(10, 10), "The quick brown fox jumps over the lazy dog.")
-#   font.size = 10
-#   font.drawText(image, vec2(10, 25), "The quick brown fox jumps over the lazy dog.")
-#   font.size = 14
-#   font.drawText(image, vec2(10, 45), "The quick brown fox jumps over the lazy dog.")
-#   font.size = 22
-#   font.drawText(image, vec2(10, 75), "The quick brown fox jumps over the lazy dog.")
-#   font.size = 36
-#   font.drawText(image, vec2(10, 110), "The quick brown fox jumps over the lazy dog.")
-#   font.size = 72
-#   font.drawText(image, vec2(10, 150), "The quick brown fox jumps over the lazy dog.")
+  font.size = 8
+  font.drawText(image, vec2(10, 10), "The quick brown fox jumps over the lazy dog.")
+  font.size = 10
+  font.drawText(image, vec2(10, 25), "The quick brown fox jumps over the lazy dog.")
+  font.size = 14
+  font.drawText(image, vec2(10, 45), "The quick brown fox jumps over the lazy dog.")
+  font.size = 22
+  font.drawText(image, vec2(10, 75), "The quick brown fox jumps over the lazy dog.")
+  font.size = 36
+  font.drawText(image, vec2(10, 110), "The quick brown fox jumps over the lazy dog.")
+  font.size = 72
+  font.drawText(image, vec2(10, 150), "The quick brown fox jumps over the lazy dog.")
 
-#   image.alphaWhite()
-#   image.save("sizes.png")
-
-
-# block:
-#   var image = newImage(800, 200, 4)
-#   var font = readFontSvg("fonts/DejaVuSans.svg")
-
-#   font.size = 16
-#   font.lineHeight = 20
-#   font.drawText(image, vec2(10, 10), readFile("sample.ru.txt"))
-
-#   image.alphaWhite()
-#   image.save("ru.png")
+  image.alphaWhite()
+  image.save("sizes.png")
 
 
-# block:
-#   var image = newImage(250, 20, 4)
+block:
+  var image = newImage(800, 200, 4)
+  var font = readFontSvg("fonts/DejaVuSans.svg")
+
+  font.size = 16
+  font.lineHeight = 20
+  font.drawText(image, vec2(10, 10), readFile("sample.ru.txt"))
+
+  image.alphaWhite()
+  image.save("ru.png")
 
 
-#   var font = readFontSvg("fonts/DejaVuSans.svg")
-#   font.size = 11 # 11px or 8pt
-#   font.drawText(image, vec2(10, 4), "The quick brown fox jumps over the lazy dog.")
+block:
+  var image = newImage(250, 20, 4)
 
-#   image = image.magnify(4)
-#   image.alphaWhite()
-#   image.save("scaledup.png")
+  var font = readFontSvg("fonts/DejaVuSans.svg")
+  font.size = 11 # 11px or 8pt
+  font.drawText(image, vec2(10, 4), "The quick brown fox jumps over the lazy dog.")
 
-
-# block:
-#   var image = newImage(140, 20, 4)
-
-#   var font = readFontSvg("fonts/DejaVuSans.svg")
-#   font.size = 11 # 11px or 8pt
-#   font.drawText(image, vec2(8, 4), "momomomomomomo")
-
-#   image = image.magnify(6)
-#   image.alphaWhite()
-#   image.save("subpixelpos.png")
+  image = image.magnify(4)
+  image.alphaWhite()
+  image.save("scaledup.png")
 
 
-# block:
-#   var image = newImage(140, 20, 4)
+block:
+  var image = newImage(140, 20, 4)
 
-#   var font = readFontSvg("fonts/DejaVuSans.svg")
-#   font.size = 11 # 11px or 8pt
-#   var glyph = font.glyphs["g"]
-#   var under = font.glyphs["_"]
+  var font = readFontSvg("fonts/DejaVuSans.svg")
+  font.size = 11 # 11px or 8pt
+  font.drawText(image, vec2(8, 4), "momomomomomomo")
 
-#   for i in 0..<10:
-#     var glyphOffset: Vec2
-#     var at = vec2(12.0 + float(i)*12, 11)
-#     var glyphImage = font.getGlyphImage(glyph, glyphOffset, quality=4, subPixelShift=float(i)/10.0)
-#     image.blit(
-#       glyphImage,
-#       rect(0, 0, glyphImage.width, glyphImage.height),
-#       rect(int(at.x + glyphOffset.x), int(at.y + glyphOffset.y), glyphImage.width, glyphImage.height)
-#     )
+  image = image.magnify(6)
+  image.alphaWhite()
+  image.save("subpixelpos.png")
 
 
-#   image = image.magnify(6)
-#   for i in 0..<10:
-#     let at = vec2(12.0 + float(i)*12, 15) * 6
-#     font.drawText(image, at + vec2(0, 6), "+0." & $i)
+block:
+  var image = newImage(140, 20, 4)
 
-#   image.alphaWhite()
+  var font = readFontSvg("fonts/DejaVuSans.svg")
+  font.size = 11 # 11px or 8pt
+  var glyph = font.glyphs["g"]
+  var under = font.glyphs["_"]
 
-#   let red = rgba(255, 0, 0, 255)
-#   for i in 0..<10:
-#     let at = vec2(12.0 + float(i)*12, 15) * 6
-#     image.line(at, at + vec2(7*6, 0), red)
-#     image.line(at + vec2(7*6, 0), at + vec2(7*6, -13*6), red)
-#     image.line(at + vec2(0, -13*6), at + vec2(7*6, -13*6), red)
-#     image.line(at + vec2(0, -13*6), at, red)
+  for i in 0..<10:
+    var glyphOffset: Vec2
+    var at = vec2(12.0 + float(i)*12, 11)
+    var glyphImage = font.getGlyphImage(glyph, glyphOffset, quality=4, subPixelShift=float(i)/10.0)
+    image.blit(
+      glyphImage,
+      rect(0, 0, glyphImage.width, glyphImage.height),
+      rect(int(at.x + glyphOffset.x), int(at.y + glyphOffset.y), glyphImage.width, glyphImage.height)
+    )
 
+  image = image.magnify(6)
+  for i in 0..<10:
+    let at = vec2(12.0 + float(i)*12, 15) * 6
+    font.drawText(image, at + vec2(0, 6), "+0." & $i)
 
-#   image.save("subpixelglyphs.png")
+  image.alphaWhite()
+
+  let red = rgba(255, 0, 0, 255)
+  for i in 0..<10:
+    let at = vec2(12.0 + float(i)*12, 15) * 6
+    image.line(at, at + vec2(7*6, 0), red)
+    image.line(at + vec2(7*6, 0), at + vec2(7*6, -13*6), red)
+    image.line(at + vec2(0, -13*6), at + vec2(7*6, -13*6), red)
+    image.line(at + vec2(0, -13*6), at, red)
+
+  image.save("subpixelglyphs.png")
 
 
 block:
