7c6efab488fe72a00bf187a7ec23ec7fb03039c0
treeform <starplant@gmail.com>
2018-08-13 09:23:31 -0700

    Added alignModes and align function.

--- src/typography/layout.nim
+++ src/typography/layout.nim
@@ -4,6 +4,7 @@ import font, rasterizer
 
 type
   Span* = object
+    ## Represents a run of litter of same size and font
     font: Font
     fontSize: float
     # lineHeight: float
@@ -11,12 +12,22 @@ type
     text: string
 
   GlyphPosition* = object
+    ## Represents a glyph position after typesetting
     font*: Font
     fontSize*: float
     #lineHeight: float
     subPixelShift*: float
     at*: Vec2
+    size*: Vec2
     character*: string
+
+  AlignMode* = enum
+    ## Text align mode
+    Left
+    Center
+    Right
+    Top
+    Bottom
 
 
 proc kerningAdjustment*(font: var Font, prev, c: string): float =
@@ -30,15 +41,20 @@ proc kerningAdjustment*(font: var Font, prev, c: string): float =
       return - kerning * scale
 
 
-
-proc typeset*(font: var Font, text: string, start: Vec2 = vec2(0, 0)): seq[GlyphPosition] =
+proc typeset*(
+    font: var Font,
+    text: string,
+    start: Vec2 = vec2(0, 0)
+  ): seq[GlyphPosition] =
   ## Draw text string
   result = @[]
   var at = start
   var prev = ""
   var fontHeight = font.ascent - font.descent
   var scale = font.size / fontHeight
-  at.y += font.size
+
+  at.y += floor(font.ascent * scale)
+
   for rune in runes(text):
     let c = $rune
     if rune == Rune(10):
@@ -52,19 +68,39 @@ proc typeset*(font: var Font, text: string, start: Vec2 = vec2(0, 0)): seq[Glyph
       var glyphOffset: Vec2
       var subPixelShift = at.x - floor(at.x)
       let pos = vec2(floor(at.x), floor(at.y + glyphOffset.y))
-      result.add GlyphPosition(
-        font: font,
-        fontSize: font.size,
-        subPixelShift: subPixelShift,
-        at: pos,
-        character: c
-      )
+      let img = font.getGlyphImage(glyph, glyphOffset, subPixelShift=subPixelShift)
+      if img.width != 0 and img.height != 0:
+        result.add GlyphPosition(
+          font: font,
+          fontSize: font.size,
+          subPixelShift: subPixelShift,
+          at: pos,
+          size: vec2(float img.width, float img.height),
+          character: c
+        )
       at.x += glyph.advance * scale
       prev = c
 
     else:
       discard
-      #echo "missing", $c
+
+proc align*(layout: var seq[GlyphPosition], alignMode: AlignMode = Left) =
+  ## Shifts layout by alignMode
+  if layout.len == 0: return
+  var maxX = layout[0].at.x
+  var minX = layout[0].at.x
+  if alignMode == Right or alignMode == Center:
+    for pos in layout:
+      maxX = max(maxX, pos.at.x + pos.size.x)
+      minX = min(minX, pos.at.x)
+    let textWidth = maxX - minX
+    if alignMode == Right:
+      for pos in layout.mitems:
+        pos.at.x -= textWidth
+    if alignMode == Center:
+      let center = textWidth / 2.0
+      for pos in layout.mitems:
+        pos.at.x -= center
 
 
 proc drawText*(image: var Image, layout: seq[GlyphPosition]) =
