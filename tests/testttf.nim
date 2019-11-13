2a6a3890aeeca240101d66bb0accb698ac88b698
treeform <starplant@gmail.com>
2019-11-13 22:40:32 +0000

    Add a test fiel for ttf

---
+++ tests/testttf.nim
@@ -0,0 +1,31 @@
+import tables
+import flippy, vmath, chroma, print
+import typography
+
+
+proc alphaWhite(image: var Image) =
+  ## Typography deals mostly with transperant images with white text
+  ## This is hard to see in tests so we convert it to white background
+  ## with black text.
+  for x in 0..<image.width:
+    for y in 0..<image.height:
+      var c = image.getrgba(x, y)
+      c.r = uint8(255) - c.a
+      c.g = uint8(255) - c.a
+      c.b = uint8(255) - c.a
+      c.a = 255
+      image.putrgba(x, y, c)
+
+
+block:
+  var font = readFontTtf("fonts/Ubuntu.ttf")
+  font.size = 16
+    
+  var image = newImage(500, 40, 4)
+
+  font.drawText(image, vec2(10, 10), "The quick brown fox jumps over the lazy dog.")
+
+  image.alphaWhite()
+  image.save("basicTtf.png")
+
+  echo "saved"
