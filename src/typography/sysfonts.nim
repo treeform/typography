66788e6a99cc0b86d92c1dc562cce8e6be31f415
Andre von Houck <starplant@gmail.com>
2020-03-06 19:42:50 +0000

    Improve sysfonts.

--- src/typography/sysfonts.nim
+++ src/typography/sysfonts.nim
@@ -1,32 +1,57 @@
-## Utilisites for finding fonts installed, or for fall backs when fonts are not found.
+## Utils for finding installed and system fonts.
 
-import strformat, ospaths, os, strutils
+import strformat, os, strutils
+
+var fontDirecotires*: seq[string]
 
 when defined(MacOSX):
-  let fontDirecotires* = @[
+  fontDirecotires = @[
     "/System/Library/Fonts/",
     "/Library/Fonts/",
     getHomeDir() & "/Library/Fonts/"
   ]
 elif defined(windows):
-  let fontDirecotires* = @[
+  fontDirecotires = @[
     r"C:\Windows\Fonts",
   ]
 else:
-  let fontDirecotires* = @[]
+  # TODO implement linux
+  discard
 
 proc getSystemFonts*(): seq[string] =
+  ## Get a list of all of the installed fonts.
   for fontDir in fontDirecotires:
     for kind, path in walkDir(fontDir):
       result.add path
 
 proc findFont*(fontName: string): string =
+  ## Find a font given a font name.
   for fontDir in fontDirecotires:
     for kind, path in walkDir(fontDir):
       let (dir, name, ext) = path.splitFile()
       if name.toLowerAscii() == fontName.toLowerAscii():
         return path
 
+proc getSystemFontPath*(): string =
+  ## Gets the path to the system font.
+  ## * San Francisco on MacOS.
+  ## * Arial on Windows.
+  ## * Linux - not implemented.
+  when defined(macOS):
+    "/System/Library/Fonts/SFNSText.ttf"
+  elif defined(windows):
+    r"C:\Windows\Fonts\arial.ttf"
+  # else defined(linux):
+  #   TODO: go through these direcotires
+  #   "~/.fonts/"
+  #   "/usr/share/fonts/truetype/"
+  #   "/usr/X11R6/lib/X11/fonts/ttfonts/"
+  #   "/usr/X11R6/lib/X11/fonts/"
+  #   "/usr/share/fonts/truetype/"
+  else:
+    return ""
+
 when isMainModule:
   echo getSystemFonts()
   echo findFont("Arial")
+  echo getSystemFontPath()
