585c707535849d4eb4eac7b4a99ea2e6edda8523
treeform <starplant@gmail.com>
2020-11-16 21:05:42 -0800

    Fixed kerning pairs from OTF.

--- src/typography/opentype/parser.nim
+++ src/typography/opentype/parser.nim
@@ -716,6 +716,14 @@ proc readFontOtf*(f: Stream): Font =
     typeface.glyphs[code] = typeface.glyphArr[glyphIndex]
     typeface.glyphs[code].code = code
 
+  font.typeface.kerning = initTable[(string, string), float32]()
+  for table in otf.kern.subTables:
+    for pair in table.kerningPairs:
+      var u1 = typeface.glyphArr[pair.left].code
+      var u2 = typeface.glyphArr[pair.right].code
+      if u1.len > 0 and u2.len > 0:
+        font.typeface.kerning[(u1, u2)] = pair.value.float32
+
   return font
 
 proc readFontOtf*(filePath: string): Font =
